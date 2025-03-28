a0d33c39d8c4e9265bb207a9b216bb07689d265d
Bruno Cabral <cabral.brc@gmail.com>
2025-03-28 11:03:14 -0700

    tell consumers about dependencies

--- pig/cmake/pigConfig.cmake
+++ pig/cmake/pigConfig.cmake
@@ -1,1 +1,17 @@
+# install SDL2
+find_package(SDL2 REQUIRED)
+find_package(SDL2_net REQUIRED)
+find_package(SDL2_image REQUIRED)
+find_package(SDL2_mixer REQUIRED)
+find_package(SDL2_ttf REQUIRED)
+find_package(SDL2_ttf REQUIRED)
+# install ffmpeg
+find_library(LIBAVCODEC avcodec REQUIRED)
+find_library(LIBAVDEVICE avdevice REQUIRED)
+find_library(LIBAVFILTER avfilter REQUIRED)
+find_library(LIBAVFORMAT avformat REQUIRED)
+find_library(LIBAVUTIL avutil REQUIRED)
+find_library(LIBSWRESAMPLE swresample REQUIRED)
+find_library(LIBSWSCALE swscale REQUIRED)
+
 include("${CMAKE_CURRENT_LIST_DIR}/pigTargets.cmake")
